0357cbaebe2ad1912c3c57bebe7ca1da206abfce
GingerbreadFred <richiwatson@gmail.com>
2025-12-28 15:20:22 +0000

    Add mac specific configs and fixup excludes per os

--- dot_config/nvim/lua/custom/plugins/init.lua
+++
@@ -1,8 +0,0 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
--- TODO: toggle header / source
--- TODO: create definition from declaration
--- TODO: figure out parsing test results from GTest in NeoTest (Does it not do this already?)
